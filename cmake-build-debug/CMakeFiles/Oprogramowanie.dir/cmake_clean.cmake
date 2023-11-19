3c605be2f0d7f6f0f49d2952d80f6fb78dbe51f8
kamilkrawczyk555 <252333@edu.p.lodz.pl>
2023-11-19 13:02:26 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Oprogramowanie.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Oprogramowanie.dir/main.cpp.o"
+  "Oprogramowanie"
+  "Oprogramowanie.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Oprogramowanie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Oprogramowanie.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Oprogramowanie.
+# This may be replaced when dependencies are built.
